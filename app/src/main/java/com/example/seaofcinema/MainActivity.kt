8bc68f065dc61e6fdc3f7e1e13ee5538d53708ea
Sava <zivaataga@gmail.com>
2025-04-22 11:01:58 +0300

    Added themes: night, day. Added styles, buttons (linked listeners)

--- app/src/main/java/com/example/seaofcinema/MainActivity.kt
+++ app/src/main/java/com/example/seaofcinema/MainActivity.kt
@@ -9,85 +9,46 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var menuItemsContainer: LinearLayout
-    private lateinit var buttonMenu: Button
-    private var isMenuOpen = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        initMenuButtons()
 
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        // Находим элементы
-        menuItemsContainer = findViewById(R.id.menuItemsContainer)
-        buttonMenu = findViewById(R.id.buttonMenu)
-
-        // Устанавливаем обработчик клика
-        buttonMenu.setOnClickListener {
-            toggleMenu()
-        }
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        initNavigation(topAppBar)
     }
 
-    fun initMenuButtons() {
-        val buttons = mapOf(
-            R.id.buttonFilms to "Фильмы Скоро будут",
-            R.id.buttonSeries to "Сериалы Скоро будут",
-            R.id.buttonSelect to "В разработке",
-            R.id.buttonWatchLater to "Скоро будет",
-            R.id.buttonSetting to "В разработке"
-        )
-
-        buttons.forEach { (id, message) ->
-            findViewById<Button>(id).setOnClickListener {
-                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    private fun initNavigation(topAppBar: MaterialToolbar) {
+        topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
             }
         }
-    }
 
-    private fun toggleMenu() {
-        val menuButtonHeight = buttonMenu.height.toFloat()
-        val delayBetweenAnimations = 100L
-
-        if (isMenuOpen) {
-            // Скрываем кнопки в обратном порядке
-            for (i in 0 until menuItemsContainer.childCount) {
-                val child = menuItemsContainer.getChildAt(i)
-                child.animate()
-                    .translationY(-menuButtonHeight - (i *child.height.toFloat()))  //
-                    .setDuration(300)
-                    .setStartDelay(i * delayBetweenAnimations) // Задержка увеличивается для каждой следующей кнопки
-                    .withEndAction {
-                        if (i == menuItemsContainer.childCount - 1) { // Когда последняя кнопка закончила свертку
-                            menuItemsContainer.visibility = View.GONE
-                        }
-                    }
-                    .start()
-
-            }
-        } else {
-            // Показываем кнопки последовательно, выезжая из-под кнопки меню
-            menuItemsContainer.visibility = View.VISIBLE
-            for (i in 0 until menuItemsContainer.childCount) {
-                val child = menuItemsContainer.getChildAt(i)
-                child.translationY = -menuButtonHeight - (i * child.height.toFloat())
-                child.animate()
-                    .translationY(0f)
-                    .setDuration(300)
-                    .setStartDelay(i * delayBetweenAnimations)
-                    .start()
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
             }
         }
-        isMenuOpen = !isMenuOpen
     }
 }

01059138c906d8d5b82a0452bc656cf0ea23f4db
Sava <zivaataga@gmail.com>
2025-06-19 09:41:53 +0300

    1.RecyclerView has been created for the main movie page 2.The main screen and the details screen have been translated into fragments. 3.Added a pop-up menu indicating that the user is about to leave the app.

--- app/src/main/java/com/example/seaofcinema/MainActivity.kt
+++ app/src/main/java/com/example/seaofcinema/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.seaofcinema
 
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -9,9 +10,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         //Зупускаем фрагмент при старте
         supportFragmentManager
